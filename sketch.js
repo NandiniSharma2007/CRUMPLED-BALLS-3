0ee839bf09a0b66df4e9393b11b13f8725e13fc8
NandiniSharma2007 <66727811+NandiniSharma2007@users.noreply.github.com>
2020-07-20 21:19:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint= Matter.Constraint;
+var paper;
+var dustbin;
+var ground;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+  
+  dustbin=new Dustbin(590,670,250,30);
+  dustbin1=new Dustbin(450,610,30,150);
+  dustbin2=new Dustbin(730.5,610,30,150);
+  
+  
+paper = new Paper(100,600,50,50);
+launcher=new Launcher(paper.body,{x:100,y:400});
+   ground = new Ground(400,700,800,30);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  ground.display();
+  
+  
+  dustbin.display();
+  dustbin1.display();
+  launcher.display();
+  dustbin2.display();
+  paper.display();
+ 
+}
+// function keyPressed() {
+//   if( keyCode === UP_ARROW){
+//     Matter.Body.applyForce(paper.body,paper.body.position,{x:60,y:-60});
+//   }
+// }
+function mouseDragged(){
+  Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY});
+
+}
+function mouseReleased(){
+  launcher.fly();
+}
+
+
